a8566bfdd2b7f1883693761b68b8d132a00e7db6
Juliana Freitas <julianachristello@gmail.com>
2021-06-03 16:13:26 -0300

    Edit

--- src/components/ModalMap.js
+++ src/components/ModalMap.js
@@ -5,6 +5,7 @@ import { IoMdClose } from "react-icons/io";
 export default function ModalMap({ openMaps, setOpenMaps, post }) {
   const { geolocation } = post;
   function closeMaps() {
+    
     setOpenMaps(false);
   }
 
